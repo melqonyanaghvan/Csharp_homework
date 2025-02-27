2a648c1df33210c0c1d3d9af20c384b83530c679
melqonyanaghvan <144158237+melqonyanaghvan@users.noreply.github.com>
2025-02-27 23:44:52 +0400

    Add files via upload

---
+++ task1/task6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       Console.Write("Enter weight in kg: ");
+        double weight = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter height in meters: ");
+        double height = double.Parse(Console.ReadLine());
+
+        double bmi = weight / (height * height);
+
+        if (bmi < 18.5){
+            Console.WriteLine($"BMI is {bmi} - Underweight");
+        }else if (bmi >= 18.5 && bmi <= 24.9) {
+            Console.WriteLine($"BMI is {bmi} - Normal weight");
+        }else if (bmi >= 25 && bmi <= 29.9){
+            Console.WriteLine($"BMI is {bmi} - Overweight");            
+        }else{
+            Console.WriteLine($"BMI: {bmi} - Obese");
+        }
+        
+    }
+}
