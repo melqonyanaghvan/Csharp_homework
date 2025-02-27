e446bef6a4f60bfce66eb7424c525ae52d953724
melqonyanaghvan <144158237+melqonyanaghvan@users.noreply.github.com>
2025-02-27 22:56:25 +0400

    Add files via upload

---
+++ task1/task2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program{
+    static void Main(){
+        Console.Write("enter the year: ");
+        int year = int.Parse(Console.ReadLine());
+
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)){
+            Console.WriteLine($"Year {year} is a leap year");
+        }else{
+            Console.WriteLine($"Year {year} is not a leap year");
+        }
+    }
+}
