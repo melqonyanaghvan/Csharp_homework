cfdd85a9b594f56ae6407f2ddab2ec3203b0fdb0
melqonyanaghvan <144158237+melqonyanaghvan@users.noreply.github.com>
2025-02-27 22:36:09 +0400

    Add files via upload

---
+++ task1/task1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+        Console.Write("Enter the number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number > 0){
+            Console.WriteLine($"Number {number} is positive.");
+        }else if (number < 0){
+            Console.WriteLine($"Number {number} is negative.");
+        }else{
+            Console.WriteLine("Number is zero.");
+        }
+    }
+}
